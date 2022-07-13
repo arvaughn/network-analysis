bcfb60d3b3972ad86262814fa9e814a96d1b50eb
arvaughn <108916113+arvaughn@users.noreply.github.com>
2022-07-13 13:35:37 -0400

    Update VaughnSNALab-2.R

--- VaughnSNALab-2.R
+++ VaughnSNALab-2.R
@@ -13,10 +13,25 @@ dlt1_ties <- read_csv("lab-2/data/dlt1-edges.csv",
                                        `Discussion ID` = col_character())) |>
   clean_names()
 
-
+##code to read csv
+##
 dlt1_actors <- read_csv("lab-2/data/dlt1-nodes.csv", 
                         col_types = cols(UID = col_character(), 
                                          Facilitator = col_character(), 
                                          expert = col_character(), 
                                          connect = col_character())) |>
   clean_names()
+
+##code to tell R that the uid is the name of the nodes
+dlt1_network <- tbl_graph(edges = dlt1_ties,
+                          nodes = dlt1_actors,
+                          node_key = "uid",
+                          directed = TRUE)
+
+dlt1_network
+plot(dlt1_network)
+autograph(dlt1_network)
+
+edge_density(dlt1_network)
+centr_degree(dlt1_network)
+reciprocity(dlt1_network)
